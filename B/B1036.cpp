a5ca4ec28f22a62b3e0fe2b61454e03fa3d84429
jay <879645920@qq.com>
2019-01-22 09:28:45 +0800

    水题

---
+++ B/B1036.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//奇数row=col/2+1,偶数row=col/2
+int main() {
+    int N;
+    char c;
+    cin >> N >> c;
+    int t = N / 2 + N % 2;
+    for (int i = 0; i < N; i++)
+        cout << c;
+    cout << endl;
+    for (int i = 0; i < t - 2; i++) {
+        cout << c;
+        for (int k = 0; k < N - 2; k++)
+            cout << " ";
+        cout << c << endl;
+    }
+    for (int i = 0; i < N; i++)
+        cout << c;
+    return 0;
+}
