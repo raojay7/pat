8336278cd4570edb69269ea4bf6e8c9fda1bed2c
raojay7 <879645920@qq.com>
2019-02-15 19:16:32 +0800

    水题

---
+++ A/twopointer/1029.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+int k[200005];
+int main() {
+    int n, m, temp, count = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &k[i]);
+    k[n + 1] = INT_MAX;
+    cin >> m;
+    int midpos = (n + m + 1) / 2, i = 1;
+    for (int j = 1; j <= m; j++) {
+        scanf("%d", &temp);
+        while (k[i] < temp) {
+            count++;
+            if (count == midpos) cout << k[i];
+            i++;
+        }
+        count++;
+        if (count == midpos) cout << temp;
+    }
+    while (i <= n) {
+        count++;
+        if (count == midpos) cout << k[i];
+        i++;
+    }
+    return 0;
+}
